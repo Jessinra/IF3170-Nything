5eca30488ce6cf1410b0db4acc8d1587b067dd27
Felix <felix.septianus@gmail.com>
2018-09-22 01:43:42 +0700

    Cache current score

--- solver/incremental_solver.py
+++ solver/incremental_solver.py
@@ -5,6 +5,7 @@ class IncrementalSolver(BaseSolver):
     def __init__(self, chess_board):
         super().__init__()
         self.chess_board = chess_board
+        self.current_score = self.evaluator.evaluate(self.chess_board)
 
     def move_chess_piece(self, chess_piece, position):
         from_position = chess_piece.position
@@ -12,7 +13,7 @@ class IncrementalSolver(BaseSolver):
         self.chess_board.update_chess_piece(chess_piece, from_position)
 
     def evaluate_move(self, moved_piece, new_position):
-
+        # Store original position as we need to move the piece for evaluation
         old_position = moved_piece.position
 
         # Try to forecast score
--- solver/simulated_annealing.py
+++ solver/simulated_annealing.py
@@ -9,19 +9,17 @@ class SimulatedAnnealing(IncrementalSolver):
         self.threshold_generator = threshold_generator
 
     def next_step(self):
-        moved_piece = random.choice(self.chess_board.pieces)
-        new_position = random.choice(self.get_empty_tiles())
-
-        current_score = self.evaluator.evaluate(self.chess_board)
+        moved_piece, new_position = self.get_random_neighbour()
         move_score = self.evaluate_move(moved_piece, new_position)
 
-        if self.is_good_move(current_score, move_score):
+        if self.is_good_move(move_score) or self.should_bad_move_be_accepted(move_score):
             self.move_chess_piece(moved_piece, new_position)
-
-        elif self.is_move_accepted_by_chances(current_score - move_score):
-            self.move_chess_piece(moved_piece, new_position)
+            self.current_score = move_score
 
         self.step += 1
+
+    def get_random_neighbour(self):
+        return random.choice(self.chess_board.pieces), random.choice(self.get_empty_tiles())
 
     def get_empty_tiles(self):
         empty_tiles = []
@@ -34,11 +32,11 @@ class SimulatedAnnealing(IncrementalSolver):
 
         return empty_tiles
 
-    def is_good_move(self, current_score, move_score):
-        return move_score > current_score
+    def is_good_move(self, move_score):
+        return move_score > self.current_score
 
-    def is_move_accepted_by_chances(self, score_difference):
-        return random.uniform(0, 1) <= self.get_threshold(score_difference, self.step)
+    def should_bad_move_be_accepted(self, bad_move_score):
+        return random.random() < self.get_threshold(self.current_score - bad_move_score, self.step)
         
     def get_threshold(self, score_difference, step):
         return self.threshold_generator.calculate_threshold(score_difference, step)
