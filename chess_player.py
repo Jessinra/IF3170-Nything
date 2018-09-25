b2fb5d2adb4222cecc70649c9e540047242d6963
JessinRa <jessin.bdg@gmail.com>
2018-09-26 00:31:08 +0700

    Minor fix

--- chess_player.py
+++ chess_player.py
@@ -1,6 +1,3 @@
-from chess_board import ChessBoard
-
-
 class ChessPlayer:
     def __init__(self, board, solver):
         self.chess_board = board
@@ -8,11 +5,7 @@ class ChessPlayer:
 
     def solve(self, iteration_count=0):
         for _ in range(iteration_count):
-            
-            # chess_piece, position = self.solver.next_step()
-            # self.move_chess_piece(chess_piece, position)
 
-            # solver return none,
             try:
                 self.solver.next_step()
             except:
