e0cd60e75b5f21c9ddd153ae43e8d087e41d19cb
kelvinkristian <kelvinkristian4@gmail.com>
2018-09-28 20:10:36 +0700

    cleaning main.py

--- main.py
+++ main.py
@@ -10,9 +10,5 @@ mutation_prob = 0.25
 solver = SolverModelFactory.create_model("genetic_algorithm", parent, mutation_prob)
 solver.generate()
 solver.next_step()
-for i in range(0, solver.population_count):
-    print(solver.population[i].position)
-    print(solver.new_parent_position[i])
-
 
 
