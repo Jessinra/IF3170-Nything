ae34643cce1fa39883e6254e8a62130ea3740025
kelvinkristian <kelvinkristian4@gmail.com>
2018-10-01 11:27:00 +0700

    refactor pranet.py name

--- solver/state_evaluator.py
+++ solver/state_evaluator.py
@@ -9,19 +9,21 @@ class StateEvaluator:
     def evaluate_piece_attack_target(self, chess_board, chess_piece):
         attack_pattern = chess_piece.get_attack_pattern()
 
-        score = 0
+        tuple_score = (0, 0)  # (same, diff)
+
         for direction in attack_pattern:
             tiles_to_check = attack_pattern[direction]
             for tile in tiles_to_check:
                 piece_on_checked_tile = chess_board.get_piece_on_tile(tile)
                 if piece_on_checked_tile is not None:
-                    score += self.get_score(piece_on_checked_tile, chess_piece)
+                    tuple_score += self.get_score(piece_on_checked_tile, chess_piece)
                     break
 
-        return score
+        single_score = tuple_score[0] + tuple_score[1]
+        return tuple_score, single_score
 
     def get_score(self, targeted_piece, chess_piece):
         if targeted_piece.color == chess_piece.color:
-            return -1  # minimize same color
+            return -1, 0  # minimize same color
         else:
-            return 1  # maximize different color
+            return 0, 1  # maximize different color
